f8ba752bea89b281e3e2e6c71fc1868e6ecdeccb
Burak Ozturk <120913735+Burak-ai@users.noreply.github.com>
2025-07-27 12:16:57 +0300

    Update XGBoost.py

--- XGBoost.py
+++ XGBoost.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from xgboost import XGBRegressor
 
 data = pd.read_csv('melbourne-housing-snapshot/melb_data.csv')
 
@@ -9,3 +10,7 @@ X = data[cols_to_use]
 y = data.Price
 
 X_train, X_valid, y_train, y_valid = train_test_split(X, y)
+
+
+my_model = XGBRegressor()
+my_model.fit(X_train, y_train)
