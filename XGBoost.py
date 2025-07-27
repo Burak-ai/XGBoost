eab5c6eab00a3fde43e4689b27dba9197132af3a
Burak Ozturk <120913735+Burak-ai@users.noreply.github.com>
2025-07-27 14:46:00 +0300

    Update XGBoost.py

--- XGBoost.py
+++ XGBoost.py
@@ -2,6 +2,9 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from xgboost import XGBRegressor
 from sklearn.metrics import mean_absolute_error
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
 
 data = pd.read_csv('melb_data.csv')
 
